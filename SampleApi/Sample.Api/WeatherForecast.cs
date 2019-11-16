cc01eeeae0e62e898b4c8a8a591db9af68d19b7b
cem-altinay <cem.altinay@outlook.com>
2019-11-16 22:00:26 +0300

    commit

--- SampleApi/Sample.Api/WeatherForecast.cs
+++ SampleApi/Sample.Api/WeatherForecast.cs
@@ -11,6 +11,7 @@ namespace Sample.Api
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
         public string Summary { get; set; }
+
         public int Test { get; set; }
     }
 }
